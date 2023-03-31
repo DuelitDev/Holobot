adc092c864cb2864b9015e8d727fdc5206acfa5d
DuelitDev <100027958+DuelitDev@users.noreply.github.com>
2023-03-31 20:02:26 +0900

    release v2.4.1.230331.6

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 # app.py
 
-import sys
-if len(sys.argv) > 1 and sys.argv[1] == "init":
-    print("Please create your global configures.")
-    while True:
-        pass
-
 
 from botlib.module import Dev, Janken, Player
 from botlib.sys.config import Config
--- botlib/sys/config.py
+++ botlib/sys/config.py
@@ -2,11 +2,23 @@
 # botlib/sys/config.py
 
 from os import environ
+from os.path import exists
 from XProperties import Properties
 
 __all__ = ["Config"]
 
 SYSTEM_CONFIG_PATH = environ.get("HOLOBOT_CONF_PATH", "global-configure.conf")
+
+
+# initialize configure
+if not exists(SYSTEM_CONFIG_PATH):
+    prop = Properties()
+    prop.load(SYSTEM_CONFIG_PATH[:-5] + "-example.conf")
+    for key in prop.keys():
+        message = input(f"{key}=")
+        if message:
+            prop[key] = message
+    prop.save(SYSTEM_CONFIG_PATH)
 
 
 class Config:
